a4b78c62006586b97aa52a486e076586e0a21022
Tim Black <timothy.c.black@gmail.com>
2018-11-30 16:27:41 -0800

    initial working app

---
+++ server.R
@@ -0,0 +1,186 @@
+###########################
+# LA GBFS Map Server Code #
+###########################
+
+library(httr)
+library(sf)
+library(leaflet)
+library(jsonlite)
+library(tidyverse)
+library(data.table)
+library(units)
+library(htmltools)
+
+### Functions
+getDocklessDevices <- function (providerName) {
+  # Sumbit GET request to Provider API for location of dockless devices
+  #
+  # Args: 
+  #   providerName: Name of provider
+  #
+  # Returns:
+  #   sf df with dockless devices
+  #
+  # API Parameters
+  url <- switch(providerName,
+                'cyclehop' = 'https://gbfs.hopr.city/api/gbfs/5/free_bike_status',
+                'bird' = 'https://mds.bird.co/gbfs/los-angeles/free_bikes',
+                'lime' = 'https://lime.bike/api/partners/v1/gbfs/los_angeles/free_bike_status.json',
+                'lyft' = 'https://s3.amazonaws.com/lyft-lastmile-production-iad/lbs/lax/free_bike_status.json',
+                'jump' = 'https://la.jumpbikes.com/opendata/free_bike_status.json',
+                'spin' = 'https://staging.spin.pm/api/gbfs/v1/los_angeles/free_bike_status')
+  r <- GET(url)
+  df <- jsonlite::fromJSON(content(r, as='text'), flatten=TRUE)
+  rdf <- df$data$bikes
+  
+  # Support for paginated endpoints
+  if(!is.null(df$max_page)){
+    
+    lastpg <- df$max_page
+    dflist <- vector(mode = 'list', length = lastpg + 1)
+    
+    for(i in seq(1, lastpg)){
+      print(i)
+      paginatedurl <- paste0(url, "?page=", i)
+      page <- GET(paginatedurl)
+      df <- jsonlite::fromJSON(content(page, as='text'), flatten=TRUE)
+      dflist[[i+1]] <- df$data$bikes
+    }
+    rdf <- rbindlist(dflist)
+    rdf <- rdf %>% mutate(lon = as.double(lon), lat = as.double(lat))
+  }
+  
+  # Format to sf df
+  bikes <- rdf %>%
+    st_as_sf(coords = c('lon','lat')) %>%
+    mutate(provider = providerName) %>%
+    select(provider, bike_id) %>%
+    st_set_crs(4326)
+  
+  return(bikes)
+}
+
+### Load Data
+neighborhoods <- st_read('data/la_neighborhoods/la_city.shp')
+cityBoundary <- neighborhoods %>% mutate(city = 'Los Angeles') %>% group_by(city) %>% summarize(do_union=TRUE)
+names(cityBoundary$geometry) <- NULL
+providerlist <- c('lyft','jump','lime','cyclehop','bird')
+
+### Server
+server <- function(input, output) {
+  
+  # Dockless Vehicles
+  allbikes <- reactive({
+    input$download
+    withProgress(message="Fetching Data...",{
+      percentage <- 0
+      allbikes <- lapply(providerlist, function(x) {
+        percentage <<- percentage + 1/length(providerlist)*100
+        incProgress(1/length(providerlist), detail = toString(x))
+        getDocklessDevices(x);
+      })
+    })
+
+    # allbikes <- lapply()
+    allbikes <- do.call('rbind', allbikes)
+    #allbikes <- allbikes[cityBoundary,] # Filter w/in LA City
+    return(allbikes)
+  })
+  # Refresh Fetch
+  observeEvent(input$download, {allbikes()})
+  # Filter by Company
+  filteredBikes <- reactive({
+    bikes <- allbikes() %>%
+      filter(provider %in% input$providerGroup)})
+
+  
+  neighborhoodCt <- reactive({
+    
+    ct <- filteredBikes() %>%
+      st_join(neighborhoods, join=st_within, left=FALSE) %>%
+      count(Name) %>%
+      st_set_geometry(NULL)
+    
+    neighborhoodCt <- neighborhoods %>%
+      left_join(ct, by='Name') %>%
+      select(-Descriptio) %>%
+      replace_na(list(n=0)) %>%
+      mutate(area = st_area(.)) %>%
+      mutate(area = units::set_units(st_area(.), mi^2)) %>%
+      mutate(density = n/area)
+    
+    return(neighborhoodCt)
+  })
+  
+  # Map
+  output$map <- renderLeaflet({
+    
+    map <- leaflet(options(leafletOptions(preferCanvas = TRUE))) %>%
+      addProviderTiles(providers$CartoDB.Positron, options = providerTileOptions(
+        minZoom=10,
+        maxZoom=18,
+        updateWhenZooming=FALSE,
+        updateWhenIdle=TRUE)) %>%
+      setView(lng=-118.329327, lat=34.0546143, zoom=13)
+    map
+    })
+  
+  # Add bikes to map
+  observe( {
+    bikes <- filteredBikes()
+    if(length(bikes) > 1){
+      pal <- colorFactor(c('#24D000', 'red', 'purple','#5DBCD2','black'),
+                       domain=c('lime', 'jump', 'lyft','cyclehop','bird'),
+                       ordered=TRUE)
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addCircleMarkers(data = bikes,
+                         radius = 2,
+                         weight = 1,
+                         stroke = TRUE,
+                         opacity = 1,
+                         fillOpacity = 0.9,
+                         color = pal(bikes$provider),
+                         fillColor = pal(bikes$provider),
+                         label = bikes$provider,
+                         popup = bikes$bike_id,
+                         group = "Devices")
+  }})
+  
+  # Hide markers at high zoom levels  
+  observe({
+    if(!is.null(input$map_zoom)){
+      if(input$map_zoom < 13){
+        
+        neighborhoodCt <- neighborhoodCt()
+        binpal <- colorBin("Purples", neighborhoodCt$density, 6, pretty = FALSE)
+        
+        labels <- sprintf(
+          "<strong>%s</strong><br/>%g devices",
+          neighborhoodCt$Name, neighborhoodCt$n
+        ) %>% lapply(htmltools::HTML)
+        
+        leafletProxy("map") %>%
+          clearShapes() %>%
+          hideGroup("Devices") %>%
+          #addPolygons(data=cityBoundary, fill=FALSE, color='#444444', weight=2, group="Bounday") %>%
+          addPolygons(data = neighborhoodCt,
+                      weight = 1,
+                      opacity = 1,
+                      color = "white",
+                      fillColor = ~binpal(density),
+                      fillOpacity = 0.7,
+                      label = labels,
+                      labelOptions = labelOptions(
+                        style = list("font-weight" = "normal", padding = "3px 8px"),
+                        textsize = "15px",
+                        direction = "auto"),
+                      highlightOptions = highlightOptions(color = "white", weight=4, bringToFront=TRUE)) #%>%
+          #addPolygons(data=cityBoundary, fill=FALSE, color='#444444', weight=2, group="Bounday")
+      
+      } else {
+        leafletProxy("map") %>%
+          clearShapes() %>%
+          showGroup("Devices")}
+    }})
+}
--- ui.R
+++ ui.R
@@ -3,25 +3,10 @@
 #######################
 
 library(leaflet)
+library(shinyWidgets)
 
-# Choices for drop-downs
-mode_vars <- c(
-  "Car, Truck, Van" = "car_pct",
-  "Public Transit" = "transit_pct",
-  "Bicycle" = "bicycle_pct",
-  "Walked" = "walk_pct"
-)
-geo_vars <- c(
-  "LA City - Tract" = "city_tract",
-  "LA County - Tract" = "county_tract"
-)
-map_color_vars <- c(
-  "Single 5-Year Estimate" = "5yr_est",
-  "Change over Time" = "time_change"
-)
-
-years <- c(2015,2014,2013,2012,2011,2010)
-
+names <- c("Lime","JUMP","Lyft","HOPR","Bird")
+colors <- c('#24D000', 'red', 'purple','#5DBCD2','black') 
 
 fluidPage(           
     div(class="outer",
@@ -33,32 +18,47 @@ fluidPage(
         # If not using custom CSS, set height of leafletOutput to a number instead of percent
         leafletOutput("map", width="100%", height="100%"),
         
-        # Shiny versions prior to 0.11 should use class = "modal" instead.
         absolutePanel(
-          id = "controls", class = "panel panel-default", fixed = TRUE,
-          draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-          width = 330, height = "auto",
+          id="controls", class="panel panel-default", fixed=TRUE,
+          draggable=TRUE, top=60, left="auto", right=20, bottom="auto",
+          width=330, height="auto",
                       
-          h3("How Does LA Get to Work?"),
+          h3("Scooters & Bikes in LA"),
                       
-          selectInput("geography","Geography",geo_vars, selected = "city_tract"),
-          selectInput("mode", "Commute Mode", mode_vars, selected = "car_pct"),
-          selectInput("maptype", "Map Color", map_color_vars, selected = "5yr_est"),
-          conditionalPanel(
-              condition = "input.maptype == '5yr_est'",
-              selectInput("yearview", "5-Year Estimate Ending In", years, selected = 2015)),
-              conditionalPanel(
-                condition = "input.maptype == 'time_change'",
-                sliderInput(inputId = "yearRange",
-                            label = "Select the Year Range",
-                            sep = "",
-                            step = 1,
-                            min = 2010, max = 2015, value = c(2010,2015))),
-              HTML("<i>This map was compiled based on data from the American Communities Survey. To see a list of
-                     currently available ACS Estimates, visit <a href='https://www.socialexplorer.com/data/metadata/'>
-                      Social Explorer</a>. To understand the differences between different types of ACS Estimates,
-                     visit <a href='https://www.census.gov/programs-surveys/acs/guidance/estimates.html'>this ACS
-                     Guidance Document</a></i>")
+          checkboxGroupInput('providerGroup', label='Filter by Company', inline=TRUE, 
+                             choices = list('Lime'='lime',
+                                            'Bird'='bird',
+                                            'JUMP'='jump',
+                                            'Lyft'='lyft',
+                                            'HOPR'='cyclehop'),
+                             selected = c('lime','jump','lyft','cyclehop','bird')),
+
+          actionButton("download", "Refresh now"),
+          
+          lapply(1:length(names), function(x) {
+            print(x)
+            n <- length(names)
+            col <- colors[x]
+            #col <- gplots::col2hex(rainbow(n)[x])
+            css_col <- paste0("#variable div.checkbox:nth-child(",x,") span{color: ", col,"; font-size: 16px; font-weight: bold}")
+            tags$style(type="text/css", css_col)
+          }),
+          checkboxGroupButtons(inputId = "Id033", 
+                               label = "Company", choices = names, direction = "vertical"),
+          awesomeCheckboxGroup(inputId = "Id022", 
+                               label = "Company", choices = names, selected = "A"),
+          checkboxGroupInput("variable", "Company", 
+                             choices = names, selected = names),
+          materialSwitch(inputId="Id054", value=TRUE,
+                         label="Bird", right=TRUE,
+                         status="primary"),
+          div()
+
+          # ,HTML("<i>This map was compiled based on data from the American Communities Survey. To see a list of
+          #        currently available ACS Estimates, visit <a href='https://www.socialexplorer.com/data/metadata/'>
+          #         Social Explorer</a>. To understand the differences between different types of ACS Estimates,
+          #        visit <a href='https://www.census.gov/programs-surveys/acs/guidance/estimates.html'>this ACS
+          #        Guidance Document</a></i>")
                       
         )
         
